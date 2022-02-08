d5ac3b06c64cb763a959c0ab3e160e19e39b5ca7
buenosisabella <coreamilisabella@gmail.com>
2022-02-07 22:23:27 -0300

    realizado com sucesso

---
+++ script.js
@@ -0,0 +1,17 @@
+let Itens =[ "Chocolate", "Açai","Refrigerante","Pizza","strogonoff"]
+console.log(Itens)
+
+Itens.unshift("Banho")
+Itens.pop()
+Itens.push("it a coisa")
+Itens.push("morena iluminada") 
+Itens.shift()
+
+
+const number=[7,5,6,3,8,9,2,1,4]
+
+number.sort((a,b) => a - b);
+// para ordenar os elementos alfabeticos crescente usamos o metodo sort()
+
+
+console.log(number)
